feae95acba80389497b0b8a49b2007a2582a1813
smkim <ksm0057@naver.com>
2022-12-03 18:50:19 +0900

    [smkim] setupProxy 추가

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = (app) => {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:1337/",
+      changeOrigin: true,
+    })
+  );
+};
